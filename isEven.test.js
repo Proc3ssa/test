0e5139a31d31dfe6cea2752fa033ebc4c1cb0756
Faisal <pros3sa@gmail.com>
2025-04-23 04:26:32 +0000

    test push

--- isEven.test.js
+++ isEven.test.js
@@ -1,9 +1,9 @@
 const isEven = require('./isEven');
 
 test('returns "is even" for even numbers', () => {
-  expect(isEven(4)).toBe('is even');
+  expect(isEven(4)).toBe('it is even');
 });
 
 test('returns "is odd" for odd numbers', () => {
-  expect(isEven(5)).toBe('is odd');
+  expect(isEven(5)).toBe('it is odd');
 });
